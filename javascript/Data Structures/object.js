9dbcf49ceaa4ad7a1251fca8359a6b56a04bcfbb
Neodevils <neoaichan@gmail.com>
2022-11-15 23:47:47 +0300

    Object

--- javascript/Data Structures/object.js
+++ javascript/Data Structures/object.js
@@ -3,12 +3,12 @@ const obj = {
 	age: 21,
 	"some-text": true,
 	sayMyName: function () {
-		console.log(this.name);
+		console.log(this.name); // neo
 	},
-};
+}; // Object of mixed types
 
-obj.hobby = "coding";
+obj.hobby = "coding"; // Add new property
 
-console.log(obj.age);
-console.log(obj["some-text"]);
-obj.sayMyName();
+console.log(obj.age); // 21
+console.log(obj["some-text"]); // true
+obj.sayMyName(); // neo
