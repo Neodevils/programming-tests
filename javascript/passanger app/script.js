ec8c62e2ae2387a69bac6a75d9c9391bd6ac5c9d
Neodevils <neoaichan@gmail.com>
2022-12-07 17:32:06 +0300

    Done!

--- javascript/passanger app/script.js
+++ javascript/passanger app/script.js
@@ -1,12 +1,18 @@
+// Variables
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
+// Increment the count
 function increment() {
 	count++;
 	countEl.innerText = count;
 }
 
+// Save the count to the browser's local storage
 function save() {
-	console.log(count);
+	let countStr = `${count} - `;
+	saveEl.textContent += countStr;
+	countEl.textContent = 0;
+	count = 0;
 }
